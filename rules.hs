7178f1ff77ffdb33ff2333ea640e7b0f638e0175
juffel <julian.dobmann@googlemail.com>
2013-07-25 15:16:39 +0200

    deleted Cell.hs

--- rules.hs
+++ rules.hs
@@ -1,6 +1,14 @@
 module Judge where
 
+import Cell
+
 -- |entscheidet je nach Status @a@ der Zelle und ihrer Anzahl
 -- lebendiger Nachbarn @b@ ueber den Status im naechsten Zyklus
 type LivingNeighboursCount = Int
 judge :: Status -> LivingNeighboursCount -> Status
+judge Dead count
+    | (count == 3) = Alive
+    | otherwise = Dead
+judge Alive count 
+    | (count == 2 || count == 3) = Alive
+    | otherwise = Dead
