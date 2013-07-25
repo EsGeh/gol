03575db8632d3c6b1ac416bce1e6fecbaf775b3b
EsGeh <SamuelGfroerer@googlemail.com>
2013-07-25 14:46:09 +0200

    corrections on module "Judge"

--- rules.hs
+++ rules.hs
@@ -1,6 +1,6 @@
-module Judge
-
+module Judge where
 
 -- |entscheidet je nach Status @a@ der Zelle und ihrer Anzahl
 -- lebendiger Nachbarn @b@ ueber den Status im naechsten Zyklus
-judge :: Status a, Int b => a -> b -> a
+type LivingNeighboursCount = Int
+judge :: Status -> LivingNeighboursCount -> Status
