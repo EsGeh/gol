d5b342e9f47fa01bdf90f5bde0982b926ee9b5f8
EsGeh <SamuelGfroerer@googlemail.com>
2013-07-26 16:54:30 +0200

    added some "deriving(Show)" lines, to simplify debugging

--- GameData.hs
+++ GameData.hs
@@ -6,8 +6,10 @@ import Data.Maybe
 type Field = Matrix Cell
 
 data Cell = Cell Status
+	deriving(Show)
 
 data Status = Dead | Alive
+	deriving(Show)
 
 -- creates a new field of dead cells
 field width height = fromMaybe (error "field matrix creation failed!") $ m $ [ take width $ repeat (Cell Dead) | i <- [0..(height-1)] ]
